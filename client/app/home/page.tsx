6a8be368182d31586e35e597b2a94c491624bc49
JasirV <jasirmon133@gmail.com>
2024-08-28 20:19:23 +0530

    Responsve

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -6,27 +6,33 @@ import Image from "next/image";
 import Arrow from "../../../assets/Arrow.png";
 import Chair1 from "../../../assets/chair1.png";
 import Chair2 from "../../../assets/cahir2.png";
-import mirror from '../../../assets/mir.png'
+import mirror from "../../../assets/mir.png";
 import { LiaShippingFastSolid } from "react-icons/lia";
 import { GiTakeMyMoney } from "react-icons/gi";
 import { TiMessages } from "react-icons/ti";
-import {products} from '../../data/productDetails'
+import { products } from "../../data/productDetails";
 import Products from "../../data/productsData";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './carouselStyles.css';
+import "./carouselStyles.css";
 import Footer from "@/components/Footer";
 
 const page = () => {
-  type Category = "Living Room" | "Bedroom" | "Dining Room" | "Office" | "Outdoor" | "Home Decor";
+  type Category =
+    | "Living Room"
+    | "Bedroom"
+    | "Dining Room"
+    | "Office"
+    | "Outdoor"
+    | "Home Decor";
   const categories: Category[] = [
     "Living Room",
     "Bedroom",
     "Dining Room",
     "Office",
     "Outdoor",
-    "Home Decor"
+    "Home Decor",
   ];
 
   const settings = {
@@ -36,7 +42,7 @@ const page = () => {
     slidesToShow: 3,
     centerPadding: "0px",
     speed: 500,
-    initialSlide: 1, 
+    initialSlide: 1,
   };
   return (
     <div>
@@ -51,8 +57,12 @@ const page = () => {
             <br /> service and often provide them as a single <br /> solution
           </h4>
           <div className="flex gap-2 ml-20 mt-10 md:gap-5">
-            <button className="bg-[#94A8BF] p-2 md:text-lg text-xs rounded-lg md:p-4">Shop Now </button>
-            <button className="bg-secondary p-2 md:text-lg text-xs rounded-lg md:p-4">Start Now</button>
+            <button className="bg-[#94A8BF] p-2 md:text-lg text-xs rounded-lg md:p-4">
+              Shop Now{" "}
+            </button>
+            <button className="bg-secondary p-2 md:text-lg text-xs rounded-lg md:p-4">
+              Start Now
+            </button>
           </div>
         </div>
         <div className=" w-1/2 h-screen  overflow-hidden hidden md:grid">
@@ -60,7 +70,11 @@ const page = () => {
         </div>
       </div>
       <div className="absolute flex-col justify-center items-center top-48 right-72  h-[430px] w-[770px] hidden md:flex">
-        <Image src={ImgBg} alt="" className="w-full h-full object-cover hidden md:flex " />
+        <Image
+          src={ImgBg}
+          alt=""
+          className="w-full h-full object-cover hidden md:flex "
+        />
         <div className="w-10/12 h-5 bg-secondary rounded-xl absolute top-96 hidden md:flex"></div>
       </div>
       <div className="flex justify-around mt-10 mb-2 4">
@@ -73,7 +87,11 @@ const page = () => {
           </h1>
         </div>
         <div>
-          <Image src={Arrow} alt="" className="rotate-180 w-40 hidden md:flex" />
+          <Image
+            src={Arrow}
+            alt=""
+            className="rotate-180 w-40 hidden md:flex"
+          />
         </div>
       </div>
 
@@ -107,41 +125,48 @@ const page = () => {
         </div>
       </div>
 
-      <div className="flex mt-10 mb-56 justify-center">
+      <div className="flex mt-10 mb-3 md:mb-56 justify-center">
         <h1 className="text-accent  font-semibold font-sans md:text-4xl text-2xl">
           This Week Featured Product
-        </h1> 
+        </h1>
       </div>
 
-      <div className="flex justify-center mb-10 h-screen">
-        <div className="bg-[#F2D8C7]  w-4/5 h-full  rounded-3xl">
-          <div className="h-1/2  flex">
-            <div className="w-1/2 h-full p-20  flex flex-col items-center">
-            <h1 className="text-textcss font-semibold font-sans text-lg">
-  Being able to sit back and relax in your favourite amchalistrulyoueofthegreatestfeelingsin life That&apos;s why we&apos;ve created a stunning range of lovingly handmade luxury armchairs, using only materials of the finest quality to ensure you have the best seat in the house.
-</h1>
-              <button className="bg-[#94A8BF] p-4 rounded-lg mt-2 ">
+      <div className="flex justify-center mb-0 md:mb-10 md:h-screen ">
+        <div className="bg-[#F2D8C7]  w-full md:w-4/5 md:h-full h-2/3 rounded-3xl">
+          <div className="h-1/2  flex md:flex-row flex-col">
+            <div className="w-full md:w-1/2 h-full p-10 md:p-20  flex flex-col items-center">
+              <h1 className="text-textcss font-semibold font-sans text-sm md:text-lg leading-snug md:leading-relaxed text-center md:text-left">
+                Being able to sit back and relax in your favourite armchair is
+                truly one of the greatest feelings in life. That's why we've
+                created a stunning range of lovingly handmade luxury armchairs,
+                using only materials of the finest quality to ensure you have
+                the best seat in the house.
+              </h1>
+
+              <button className="bg-[#94A8BF] p-2 md:p-4 text-xs md:text-sm rounded-lg mt-2">
                 Buy Now
               </button>
             </div>
-            <div className="w-1/2">
+            <div className="w-1/2 hidden md:flex">
               <div className="bg-secondary w-96 h-[570px] rounded-t-full relative bottom-52 left-72 ">
                 <Image src={Chair1} alt="" />
               </div>
             </div>
           </div>
           <div className="h-1/2  flex ">
-            <div className="w-1/2">
+            <div className="w-1/2 hidden md:flex">
               <div className="bg-secondary w-96 h-[540px] rounded-t-full relative bottom-24 right-10 mb-10 mt-20 ">
                 <Image src={Chair2} alt="" className="scale-x-[-1]" />
               </div>
             </div>
-            <div className="w-1/2 p-20 flex flex-col items-center ">
-              
-<h1 className="text-textcss font-semibold font-sans text-lg">
-  As in the best seat in the house. Combining comfort and creativity, our smashing range of armchairs features designs to suit any style. Perhaps you&apos;re looking for a mid-century masterpiece.
-</h1>
-              <button className="bg-secondary p-4 rounded-lg mt-5 ">
+            <div className="w-full md:w-1/2 p-10 md:p-20 flex flex-col items-center ">
+              <h1 className="text-textcss font-semibold font-sans text-sm md:text-lg leading-snug md:leading-relaxed text-center md:text-left">
+                As in the best seat in the house. Combining comfort and
+                creativity, our smashing range of armchairs features designs to
+                suit any style. Perhaps you&apos;re looking for a mid-century
+                masterpiece.
+              </h1>
+              <button className="bg-secondary p-2 md:p-4 text-xs md:text-sm rounded-lg mt-2 ">
                 Buy Now
               </button>
             </div>
@@ -149,86 +174,107 @@ const page = () => {
         </div>
       </div>
 
-      <div className="flex justify-around mt-64 mb-24">
-        <div>
+      <div className="hidden justify-around mt-0 md:mt-64 mb-24 md:flex ">
+        <div className="md:flex hidden">
           <Image src={Arrow} alt="" className=" w-40" />
         </div>
         <div>
-          <h1 className="text-accent font-semibold font-sans text-4xl">
+          <h1 className="text-accent font-semibold font-sans md:text-4xl text-xl">
             Wooden Sofa Sets
           </h1>
         </div>
         <div>
-          <Image src={Arrow} alt="" className="rotate-180 w-40" />
+          <Image
+            src={Arrow}
+            alt=""
+            className="rotate-180 w-40 md:flex hidden"
+          />
         </div>
       </div>
 
-      <div className=" w-full mt-28 flex justify-center items-center  relative">
-        <div className="w-11/12 m-auto h-full ">
-          <div className="slider-container h-full ">
-            <Slider {...settings}>
-              {Products?.map((i) => (
-                  <div key={i.index} className="flex justify-center items-center">
-                    <Image
-                      className=" sm:h-44 sm:w-44 rounded-full"
-                      src={i.img as string}
-                      // src={i.img}
-                      alt={i.index.toString()}
-                    />
-                  </div>
-              ))}
-            </Slider>
-          </div>
-          </div>
-    </div>
-
-
-    <div className="flex justify-around mt-14  mb-14">
-          <h1 className="text-accent font-semibold font-sans text-4xl">
-          A Huge Collection Of Furniture
-          </h1>
-      </div>
-      <div className=" w-11/12 m-auto h-10 flex justify-between items-center mb-10">
-      {categories.map((i,index)=>(
-        <div key={index} className="border-[#F7D8C4] border p-2 flex justify-center items-center">  
-          <div  className="bg-[#E5C8B2] w-40 rounded-xl text-center items-center flex p-3 justify-center text-textcss font-semibold font-sans">{i}</div>
-          </div>
-      ))}
-      </div>
-
-      <div className=" w-full h-screen flex justify-center items-center">
-      <div className="flex flex-wrap w-11/12 justify-center gap-14">
-        {products.map((product) => (
-          <div key={product.id} className="w-60 border h-72 flex flex-col items-center p-4 border-textcss">
-            <Image 
-              src={product.id} 
-              alt={product.name} 
-              width={240} 
-              height={160} 
-              layout="intrinsic"
-              objectFit="cover"
+      <div className="w-full mt-28  flex-col items-center relative md:flex hidden">
+  <div className="w-full md:w-11/12 m-auto h-full">
+    <div className="slider-container h-full">
+      <Slider {...settings}>
+        {Products?.map((i) => (
+          <div key={i.index} className="flex justify-center items-center">
+            <Image
+              className="h-40 w-40 sm:h-44 sm:w-44 md:h-52 md:w-52 rounded-full"
+              src={i.img as string}
+              alt={i.index.toString()}
             />
-            <h2 className="mt-2 text-lg font-semibold text-accent">{product.name}</h2>
-            <p className="text-xl font-bold mt-1 text-textcss">${product.price.toFixed(2)}</p>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
+  </div>
+</div>
 
-    <div className="w-full bg-redd-500  flex  mt-10 mb-16 ">
-      <div className="w-1/2 p-5 flex justify-end">
-      <Image src={mirror} alt='mirror' className="w-96 min-h-96" />
-      </div>
-      <div className="w-1/2 h-full  flex flex-col justify-center gap-12 p-14 ">
-      <h1 className="text-3xl text-textcss ">We Are The Bigest
-      Furniture Brands</h1>
-      <h1 className="text-xl text-textcss ">over the yesrs we hane ahwires been at the boc of
-thefurmc.rolhaustasaoundmewonu,thadwy
-evew yoee besieves is the auasky ef our producis</h1>
-<button className="bg-[#94A8BF] p-4 w-1/5 rounded-lg"> Shop More</button>
+<div className="flex flex-col items-center md:mt-14 mt-5 mb-14">
+  <h1 className="text-accent font-semibold font-sans md:text-4xl text-2xl text-center">
+    A Huge Collection Of Furniture
+  </h1>
+</div>
+
+<div className="w-full md:w-11/12 m-auto flex flex-wrap justify-center gap-4 md:gap-6 mb-10">
+  {categories.map((i, index) => (
+    <div
+      key={index}
+      className="border-[#F7D8C4] border p-2 flex justify-center items-center"
+    >
+      <div className="bg-[#E5C8B2] w-24 sm:w-32 md:w-40 rounded-xl text-center flex items-center justify-center p-2 sm:p-3 text-xs sm:text-sm md:text-base text-textcss font-semibold font-sans">
+        {i}
       </div>
     </div>
-    <Footer/>
+  ))}
+</div>
+
+<div className="w-full flex flex-col items-center md:items-start md:h-screen">
+<div className="flex flex-wrap w-full md:w-11/12 justify-center gap-6 sm:gap-8 md:gap-14">
+    {products.map((product) => (
+      <div
+        key={product.id}
+        className="w-36 sm:w-48 md:w-60 border h-60 sm:h-64 md:h-72 flex flex-col items-center p-2 sm:p-3 md:p-4 border-textcss"
+      >
+        <Image
+          src={product.id}
+          alt={product.name}
+          width={240}
+          height={160}
+          layout="intrinsic"
+          objectFit="cover"
+        />
+        <h2 className="mt-2 text-xs sm:text-sm md:text-lg font-semibold text-accent text-center">
+          {product.name}
+        </h2>
+        <p className="text-sm sm:text-lg md:text-xl font-bold mt-1 text-textcss">
+          ${product.price.toFixed(2)}
+        </p>
+      </div>
+    ))}
+  </div>
+
+</div>
+
+      <div className="w-full bg-redd-500  flex  mt-10 mb-16 ">
+        <div className="w-1/2 p-5  justify-end md:flex hidden">
+          <Image src={mirror} alt="mirror" className="w-96 min-h-96" />
+        </div>
+        <div className="w-full md:w-1/2 h-full flex flex-col justify-center gap-6 md:gap-12 p-6 md:p-14">
+          <h1 className="text-lg md:text-3xl text-textcss">
+            We Are The Biggest Furniture Brands
+          </h1>
+          <h1 className="text-sm md:text-xl text-textcss">
+            Over the years, we have always been at the forefront of the
+            furniture industry, ensuring that every product we create reflects
+            the highest quality.
+          </h1>
+          <button className="bg-[#94A8BF] p-2 md:p-4 w-2/5 sm:w-1/3 md:w-1/5 rounded-lg">
+            Shop More
+          </button>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CiBag1, CiSearch } from 'react-icons/ci'
+import { TiThMenuOutline } from "react-icons/ti";
 
 const NavBar = () => {
 
@@ -7,17 +8,20 @@ const NavBar = () => {
     <div className='mb-5'>
       <nav className='flex justify-between mt-6'>
         <div className=' w-1/4 flex justify-center items-center'>
-          <h1 className='text-accent font-sans font-semibold text-lg'>FurniTrove</h1>
+          <h1 className='text-accent font-sans font-semibold text-lg mx-2 md:mx-0'>FurniTrove</h1>
         </div>
-        <div className='flex  w-10/12 justify-center gap-16 items-center text-accent'>
+        <div className='hidden  w-10/12 justify-center gap-16 items-center text-accent md:flex'>
           <h1>Home</h1>
           <h1>Reviews</h1>
           <h1>Contact</h1>
           <h1>About us</h1>
         </div>
-        <div className='flex   w-1/4 justify-center gap-4 items-center text-accent'>
+        <div className='md:flex hidden w-1/4 justify-center gap-4 items-center text-accent'>
         <CiSearch size={35}/>
         <CiBag1 size={35}/>
+        </div>
+        <div className='flex md:hidden text-accent mx-2'>
+        <TiThMenuOutline size={20} />
         </div>
       </nav>
     </div>
