4819bb4ae230874001f3ed7bc3cce0fd349568c2
JasirV <jasirmon133@gmail.com>
2024-11-12 17:50:47 +0530

    updateProdcutPage

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -12,20 +12,17 @@ interface Product {
   image: string;
   productName: string;
   price: number;
-  description:string
+  description: string;
 }
 
 const Page = () => {
-  // State to store products data
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
-  // State for managing modal visibility and selected product
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-  // Fetch products data on mount
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -40,21 +37,15 @@ const Page = () => {
 
     fetchProducts();
   }, []);
-  console.log(products)
 
-  // If still loading, show a loading state
   if (isLoading) return <div>Loading...</div>;
-
-  // If there was an error, show an error message
   if (error) return <div>{error}</div>;
 
-  // Open the modal with the selected product
   const openModal = (product: Product) => {
     setSelectedProduct(product);
     setIsModalOpen(true);
   };
 
-  // Close the modal
   const closeModal = () => {
     setIsModalOpen(false);
     setSelectedProduct(null);
@@ -62,6 +53,7 @@ const Page = () => {
 
   const addToCart = () => {
     if (localStorage.getItem('token')) {
+      // Logic for adding the product to the cart
     } else {
       router.push('/login'); 
     }
@@ -69,12 +61,10 @@ const Page = () => {
 
   return (
     <div className="min-h-screen">
-      {/* NavBar Section */}
       <div className="sticky top-0 z-10 w-full">
         <NavBar />
       </div>
 
-      {/* Products Section */}
       <div className="w-full flex flex-col items-center md:h-full pt-16 px-4 md:px-0">
        <h1 className="text-4xl font-bold mb-6 text-textcss"> Our Products </h1>
         <div className="flex flex-wrap w-full md:w-11/12 justify-center gap-6 sm:gap-8 md:gap-14">
@@ -103,7 +93,6 @@ const Page = () => {
         </div>
       </div>
 
-      {/* Modal */}
       {isModalOpen && selectedProduct && (
         <Modal product={selectedProduct} onClose={closeModal} addToCart={addToCart} />
       )}
@@ -118,25 +107,24 @@ const Page = () => {
 interface ModalProps {
   product: Product;
   onClose: () => void;
+  addToCart: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ product, onClose,addToCart}) => {
-
+const Modal: React.FC<ModalProps> = ({ product, onClose, addToCart }) => {
   return (
     <div
       className="fixed inset-0 bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50"
-      onClick={onClose} // Close modal when clicking outside
+      onClick={onClose}
     >
       <div
-        className=" backdrop-blur-sm p-8  shadow-lg bg-primary rounded-lg md:w-1/2 cursor-auto"
-        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+        className="backdrop-blur-sm p-8 shadow-lg bg-primary rounded-lg md:w-1/2 cursor-auto"
+        onClick={(e) => e.stopPropagation()}
       >
         <div className="flex justify-end">
-            <h1  onClick={onClose} className="text-xl font-semibold text-textcss md:mr-7 cursor-pointer">x</h1>
+          <h1 onClick={onClose} className="text-xl font-semibold text-textcss md:mr-7 cursor-pointer">x</h1>
         </div>
         <h2 className="text-xl font-semibold mb-4 text-textcss text-center">Product Details</h2>
         <div className="flex flex-col items-center">
-          {/* Modal Content using the passed product props */}
           <Image
             src={product.image}
             alt={product.productName}
@@ -147,7 +135,7 @@ const Modal: React.FC<ModalProps> = ({ product, onClose,addToCart}) => {
           />
           <p className="mt-4 text-lg text-textcss">{product.productName}</p>
           <p className="text-xl font-bold mt-2 text-textcss">${product.price.toFixed(2)}</p>
-          <p className="text-sm  mt-2 text-textcss">${product.description}</p>
+          <p className="text-sm mt-2 text-textcss">{product.description}</p>
           <button 
             className="bg-[#94A8BF] p-1 mt-2 text-xs rounded-lg md:p-3"
             onClick={addToCart}
